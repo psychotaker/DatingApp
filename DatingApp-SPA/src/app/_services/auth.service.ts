5e419d0715d998c8d1465e6466d978ab252916b1
Teufik Cajlakovic <teufik97@gmail.com>
2019-12-04 22:31:30 +0100

    End of Section 6

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,8 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   private readonly baseUrl = 'http://localhost:5000/api/auth/';
+  private readonly jwtHelper = new JwtHelperService();
+  decodedToken: any;
 
   login(model: any) {
     return this.http.post(this.baseUrl + 'login', model)
@@ -18,6 +21,8 @@ export class AuthService {
           const user = response;
           if (user) {
             localStorage.setItem('token', user.token);
+            this.decodedToken = this.jwtHelper.decodeToken(user.token);
+            console.log(this.decodedToken);
           }
         })
       );
@@ -25,5 +30,10 @@ export class AuthService {
 
   register(model: any) {
     return this.http.post(this.baseUrl + 'register', model);
+  }
+
+  logedin() {
+    const token = localStorage.getItem('token');
+    return !this.jwtHelper.isTokenExpired(token);
   }
 }
